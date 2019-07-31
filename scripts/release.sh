79a98e057c5e14f084bb03bc4b0103bf4b35ebcc
David Ko <dk577@cornell.edu>
2019-07-30 22:45:47 -0400

    token in param store

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,11 @@
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-release-it patch --preRelease=$branch --ci
+
+get_token() {
+  echo "$(aws ssm get-parameter --name "githubaccesstoken" --query Parameter.Value --region us-east-1)"
+}
+
+token=$(get_token)
+echo "token=$token"
+
+export GITHUB_TOKEN=$token
+echo "$( release-it patch --preRelease=$branch --ci)"
