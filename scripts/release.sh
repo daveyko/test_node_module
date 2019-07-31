ffc54eb0f67c8ee0249b03e5564c4b9f9b3413a3
David Ko <dk577@cornell.edu>
2019-07-30 23:36:24 -0400

    test

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,4 +14,4 @@ token=$(get_token)
 echo "token=$token branch=$branch"
 
 # echo "$( GITHUB_TOKEN=$token release-it patch --preRelease=$branch --ci)"
-echo "$( GITHUB_TOKEN=$token npm run testrelease)"
+echo "$( GITHUB_TOKEN="6d8ba0eee99e201a9de2e88a11885f53b4713cc9" npm run testrelease)"
