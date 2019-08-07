ab4cf9b7faa5221995191d84bf2fc4be894f37db
Paintzen Admin <admin+github@@paintzen.com>
2019-08-06 21:56:59 -0400

    PROD-SUPPORT: fix script for prod-support.

--- scripts/master-release.sh
+++ scripts/master-release.sh
@@ -31,7 +31,7 @@ export GITHUB_TOKEN=$token
 if [ $branch = "develop" ]
 then 
     echo "$(release-it major --ci)"
-elif [ $branch = "production-support" ]
+elif [ $branch = "prod-support" ]
 then 
     echo "$(release-it minor --ci)"
 elif [ $branch = "hotfix" ]
