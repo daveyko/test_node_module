fb31e2be0da7e369785b5dbe56271f797d978c0c
Paintzen Admin <admin+github@@paintzen.com>
2019-08-06 21:54:32 -0400

    PROD-SUPPORT: fix script.

--- scripts/master-release.sh
+++ scripts/master-release.sh
@@ -27,16 +27,16 @@ echo "BRANCH: $branch"
 
 echo "githubaccestoken = $token currentbranch = $branch"
 
-#export GITHUB_TOKEN=$token
-#if [ $branch = "develop" ]
-#then 
-#    echo "$(release-it major --ci)"
-#elif [ $branch = "production-support" ]
-#then 
-#    echo "$(release-it minor --ci)"
-#elif [ $branch = "hotfix" ]
-#then 
-#    echo "$(release-it patch --ci)"
-#else 
-#    echo "Please enter a valid branch name: develop, production-support, or hotfix"
-#fi
+export GITHUB_TOKEN=$token
+if [ $branch = "develop" ]
+then 
+    echo "$(release-it major --ci)"
+elif [ $branch = "production-support" ]
+then 
+    echo "$(release-it minor --ci)"
+elif [ $branch = "hotfix" ]
+then 
+    echo "$(release-it patch --ci)"
+else 
+    echo "Please enter a valid branch name: develop, production-support, or hotfix"
+fi
