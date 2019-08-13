81db1d78797932dfbdb8a6e68835b39a8bb91e30
David Ko <dk577@cornell.edu>
2019-08-13 14:21:18 -0400

    bla

--- index.js
+++ index.js
@@ -2,36 +2,6 @@ const testFunc = () => {
     console.log('testyboi!')
 }
 
-const wat = () => { 
-    console.log('wat!')
-}
-
-const versionC = () => { 
-    console.log('versionC!')
-}
-
-const featureD = () => { 
-    console.log('featured')
-}
-
-const e = () => { 
-    console.log('e')
-}
-
-const f = () => { 
-    console.log('f')
-}
-
-const g = () => { 
-    console.log('e')
-}
-
 module.exports = { 
     testFunc, 
-    wat, 
-    versionC
-    featureD, 
-    e,
-    f
-    g
 }
